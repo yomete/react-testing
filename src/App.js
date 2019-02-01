223ee573e9b0b40850b5f0f2aba9ed6038753955
Yomi Eluwande <yomi@flutterwavego.com>
2019-02-01 04:59:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: 0,
+    }
+  }
+
+  setOn = event => {
+    this.setState({status: 1})
+  }; 
+
+  setOff = event => {
+    this.setState({status: 0})
+  }; 
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p className="value">
+          {this.state.status}
+        </p>
+
+        <button id="offButton" onClick={this.setOff}>Off</button>
+        <button id="onButton" onClick={this.setOn}>On</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from './App';
+var assert = require('assert');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should return -1 when the value is not present', function() {
+  assert.equal([1,2,3].indexOf(4), -1);
+});
+
+describe('Enzyme Tests', () => {
+  it('App loads with initial state of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('0');
+  });
+
+  it('on button works as expected', () => {
+    const wrapper = shallow(<App />);
+    const onButton = wrapper.find('button#onButton');
+    onButton.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('1');
+  });
+
+  it('off button works as expected', () => {
+    const wrapper = shallow(<App />);
+    const offButton = wrapper.find('button#offButton');
+    offButton.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('0');
+  });
+});
+
+describe('Jest Tests', () => {
+  it('snapshot matches', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
